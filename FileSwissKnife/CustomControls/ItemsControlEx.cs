52e0b64d84efecdb861477714874487b8aa2f53e
EmilianoElMariachi <milhouzelatarlouze@msn.com>
2020-12-15 23:17:54 +0100

    Auto-scroll vers le dernier élément ajouté à la HashView

---
+++ FileSwissKnife/CustomControls/ItemsControlEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Windows.Controls;
+
+namespace FileSwissKnife.CustomControls
+{
+    public class ItemsControlEx : ItemsControl
+    {
+
+        public event EventHandler<NotifyCollectionChangedEventArgs> ItemsChanged;
+
+        protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnItemsChanged(e);
+
+            ItemsChanged?.Invoke(this, e);
+        }
+
+    }
+}
--- FileSwissKnife/Views/HashView.xaml.cs
+++ FileSwissKnife/Views/HashView.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace FileSwissKnife.Views
+﻿using System.Collections.Specialized;
+
+namespace FileSwissKnife.Views
 {
     /// <summary>
     /// Interaction logic for HashView.xaml
@@ -8,6 +10,13 @@
         public HashView()
         {
             InitializeComponent();
+        }
+
+        private void OnItemsChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            // Scroll to the bottom on items added
+            if (e.NewItems != null && e.NewItems.Count > 0)
+                ScrollViewer.ScrollToEnd();
         }
     }
 }
